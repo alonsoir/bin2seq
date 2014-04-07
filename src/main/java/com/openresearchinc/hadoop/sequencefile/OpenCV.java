d6bcda4d0396926a58fa3812e0eb71d10c99cd3c
Qiming He <Qiming.He@openresearchinc.com>
2014-04-07 15:06:33 -0400

    refactor to detect multiple target, like face, eyes

--- src/main/java/com/openresearchinc/hadoop/sequencefile/OpenCV.java
+++ src/main/java/com/openresearchinc/hadoop/sequencefile/OpenCV.java
@@ -1,7 +1,11 @@
 package com.openresearchinc.hadoop.sequencefile;
 
+import static com.googlecode.javacv.cpp.opencv_core.CV_AA;
 import static com.googlecode.javacv.cpp.opencv_core.cvClearMemStorage;
+import static com.googlecode.javacv.cpp.opencv_core.cvGetSeqElem;
 import static com.googlecode.javacv.cpp.opencv_core.cvLoad;
+import static com.googlecode.javacv.cpp.opencv_core.cvPoint;
+import static com.googlecode.javacv.cpp.opencv_core.cvRectangle;
 import static com.googlecode.javacv.cpp.opencv_objdetect.CV_HAAR_DO_CANNY_PRUNING;
 import static com.googlecode.javacv.cpp.opencv_objdetect.cvHaarDetectObjects;
 
@@ -9,8 +13,6 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
-import java.security.MessageDigest;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
@@ -28,62 +30,64 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.googlecode.javacv.cpp.opencv_core.CvMemStorage;
+import com.googlecode.javacv.cpp.opencv_core.CvRect;
+import com.googlecode.javacv.cpp.opencv_core.CvScalar;
 import com.googlecode.javacv.cpp.opencv_core.CvSeq;
 import com.googlecode.javacv.cpp.opencv_core.IplImage;
 import com.googlecode.javacv.cpp.opencv_objdetect.CvHaarClassifierCascade;
 
 //@formatter:off
 /**
- * (Deprecated) OpenCV 
- * cp  <opencv-path>/release/lib/libopencv_java248.so 
- *            <hadoop-path>/lib/native/. otherwise will get following problem
- *            java.lang.UnsatisfiedLinkError: no opencv_java248 in
- *            java.library.path
+ * (Deprecated) OpenCV cp <opencv-path>/release/lib/libopencv_java248.so
+ * <hadoop-path>/lib/native/. otherwise will get following problem
+ * java.lang.UnsatisfiedLinkError: no opencv_java248 in java.library.path
  * 
- * JavaCV: 
- * 1. download and extract both javacv-0.7-bin.zip andjavacv-0.7-cppjars.zip 
- * 2. In Eclipse: create a user library JavaCV containing 
- * 		1)javacv.jar 2)javacpp.jar 3) javacv-<plaform>-<os>.jar 4) ffmpeg-<ver>-<platform>.jar 5) opencv-<ver>-<platform>.jar 
+ * JavaCV: 1. download and extract both javacv-0.7-bin.zip
+ * andjavacv-0.7-cppjars.zip 2. In Eclipse: create a user library JavaCV
+ * containing 1)javacv.jar 2)javacpp.jar 3) javacv-<plaform>-<os>.jar 4)
+ * ffmpeg-<ver>-<platform>.jar 5) opencv-<ver>-<platform>.jar
  * 
  * @author heq
  */
-//@formatter:on
+// @formatter:on
 public class OpenCV {
 	private static final Logger logger = LoggerFactory.getLogger(OpenCV.class);
 	private static final Configuration conf = new Configuration();
 
 	public static void main(String[] args) throws Exception {
 		String usage = "Usage: hadoop jar ./target/*.jar com.openresearchinc.hadoop.sequencefile.OpenCV [-file <input-uri-image>|-dir <input-uri-images>] -ext <ext> ";
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+		String[] otherArgs = new GenericOptionsParser(conf, args)
+				.getRemainingArgs();
 
 		List<String> argList = Arrays.asList(otherArgs);
-		String uri;		
+		String uri;
 		if (argList.indexOf("-file") != -1) {
 			uri = otherArgs[argList.indexOf("-file") + 1];
 			if (uri.toLowerCase().matches(".*png.*|.*jpg.*|.*gif.*")) {
 				detectFaceinPngJpgEtc(uri);
 			} else if (uri.toLowerCase().matches(".*ppm.*")) {
 				detectFaceInPPM(uri);
-			}else{
+			} else {
 				System.err.println("unsupported image format");
-				System.exit(2);				
-			}				
-		}else if (argList.indexOf("-dir") != -1) {
-			uri = otherArgs[argList.indexOf("-dir") + 1];
-			if (argList.indexOf("-ext") !=-1){				
-				String ext= otherArgs[argList.indexOf("-ext") + 1];
-				detectFacesInDir(uri,ext);
-			}else{
-				System.err.println(usage);
-				System.exit(2);				
+				System.exit(2);
 			}
-		}else{
+		} else if (argList.indexOf("-dir") != -1) {
+			uri = otherArgs[argList.indexOf("-dir") + 1];
+			if (argList.indexOf("-ext") != -1) {
+				String ext = otherArgs[argList.indexOf("-ext") + 1];
+				detectFacesInDir(uri, ext);
+			} else {
+				System.err.println(usage);
+				System.exit(2);
+			}
+		} else {
 			System.err.println(usage);
-			System.exit(2);			
+			System.exit(2);
 		}
 	}
 
-	public static void detectFacesInDir(String dir, String ext) throws Exception {
+	public static void detectFacesInDir(String dir, String ext)
+			throws Exception {
 		List<String> fileURIs = Util.listFiles(dir, ext);
 		for (String uri : fileURIs) {
 			logger.info(uri);
@@ -108,10 +112,12 @@ public class OpenCV {
 	public static void detectFace(String hdfsdir, String ext) throws Exception {
 		if (hdfsdir.contains("hdfs://")) {
 			conf.set("fs.defaultFS", hdfsdir);
-			FileStatus[] status = FileSystem.get(conf).listStatus(new Path(hdfsdir));
+			FileStatus[] status = FileSystem.get(conf).listStatus(
+					new Path(hdfsdir));
 			for (int i = 0; i < status.length; i++) {
 				Path path = status[i].getPath();
-				if (path.getName().endsWith(ext) && status[i].getLen() > 0 && !status[i].isSymlink()) {
+				if (path.getName().endsWith(ext) && status[i].getLen() > 0
+						&& !status[i].isSymlink()) {
 					logger.debug(status[i].getPath() + " " + status[i].getLen());
 					detectFaceinPngJpgEtc(path.toString());
 				}
@@ -136,7 +142,8 @@ public class OpenCV {
 		Map<Text, byte[]> imagesequnce = Util.readSequenceFile(uri);
 		for (Map.Entry<Text, byte[]> entry : imagesequnce.entrySet()) {
 			String filename = entry.getKey().toString();
-			BufferedImage rawimage = ImageIO.read(new ByteArrayInputStream(entry.getValue()));
+			BufferedImage rawimage = ImageIO.read(new ByteArrayInputStream(
+					entry.getValue()));
 			int faces = detectFace(rawimage);
 			logger.info("filename=" + filename + " faces=" + faces);
 			return faces;
@@ -155,7 +162,9 @@ public class OpenCV {
 		Map<Text, byte[]> imagesequnce = Util.readSequenceFile(uri);
 		for (Map.Entry<Text, byte[]> entry : imagesequnce.entrySet()) {
 			String filename = entry.getKey().toString();
-			ImageInputStream iis = ImageIO.createImageInputStream(new ByteArrayInputStream(entry.getValue()));
+			ImageInputStream iis = ImageIO
+					.createImageInputStream(new ByteArrayInputStream(entry
+							.getValue()));
 			BufferedImage rawimage = PPMImageReader.read(iis);
 			int faces = detectFace(rawimage);
 			logger.debug("filename=" + filename + "faces=" + faces);
@@ -164,17 +173,42 @@ public class OpenCV {
 		return 0;
 	}
 
-	static int detectFace(BufferedImage rawimage) throws Exception {
-		String faceClassifierPath = new File(OpenCV.class.getResource("/haarcascade_frontalface_alt.xml").getFile())
+	public static int detectFace(BufferedImage rawimage) throws Exception {
+		String faceClassifierPath = new File(OpenCV.class.getResource(
+				"/haarcascade_frontalface_alt.xml").getFile())
 				.getAbsolutePath();
+		int faces = detect(rawimage, faceClassifierPath);
+		logger.info("faces=" + faces);
+		return faces;
+	}
 
-		CvHaarClassifierCascade classifier = new CvHaarClassifierCascade(cvLoad(faceClassifierPath));
+	public static int detectEye(BufferedImage rawimage) throws Exception {
+		String eyeClassifierPath = new File(OpenCV.class.getResource(
+				"/haarcascade_eye.xml").getFile()).getAbsolutePath();
+		int eyes = detect(rawimage, eyeClassifierPath);
+		logger.info("eyes=" + eyes);
+		return eyes;
+	}
+
+	private static int detect(BufferedImage rawimage, String classifierPath) {
+		CvHaarClassifierCascade classifier = new CvHaarClassifierCascade(
+				cvLoad(classifierPath));
 		IplImage origImg = IplImage.createFrom(rawimage);
 		CvMemStorage storage = CvMemStorage.create();
-		CvSeq faces = cvHaarDetectObjects(origImg, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+		CvSeq target = cvHaarDetectObjects(origImg, classifier, storage, 1.1,
+				3, CV_HAAR_DO_CANNY_PRUNING);
 		cvClearMemStorage(storage);
-		logger.info("# faces=" + faces.total());
-		return faces.total();
+
+		for (int i = 0; i < target.total(); i++) {
+			CvRect r = new CvRect(cvGetSeqElem(target, i));
+			logger.debug("x=" + r.x() + " y=" + r.y() + " x+w=" + r.x()
+					+ r.width() + " y+h=" + r.y() + r.height());
+		}
+		logger.debug("image width=" + rawimage.getWidth() + " height="
+				+ rawimage.getHeight() + " num of identified object="
+				+ target.total());
+
+		return target.total();
 	}
 
 }
