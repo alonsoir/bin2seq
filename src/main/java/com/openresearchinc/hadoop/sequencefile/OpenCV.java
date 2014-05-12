9172d422054d5306f0f3d008eeeb41f38a1c7794
Qiming He <Qiming.He@openresearchinc.com>
2014-05-12 19:54:25 -0400

    change to run as m/r jobs

--- src/main/java/com/openresearchinc/hadoop/sequencefile/OpenCV.java
+++ src/main/java/com/openresearchinc/hadoop/sequencefile/OpenCV.java
@@ -11,20 +11,29 @@ import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 import javax.imageio.ImageIO;
 import javax.imageio.stream.ImageInputStream;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
-import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.ArrayPrimitiveWritable;
+import org.apache.hadoop.io.BytesWritable;
+import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapred.FileOutputFormat;
+import org.apache.hadoop.mapred.JobClient;
+import org.apache.hadoop.mapred.JobConf;
+import org.apache.hadoop.mapred.MapReduceBase;
+import org.apache.hadoop.mapred.Mapper;
+import org.apache.hadoop.mapred.OutputCollector;
+import org.apache.hadoop.mapred.Reducer;
+import org.apache.hadoop.mapred.Reporter;
+import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.mapred.TextOutputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -36,225 +45,128 @@ import com.googlecode.javacv.cpp.opencv_objdetect.CvHaarClassifierCascade;
 
 //@formatter:off
 /**
- * (Deprecated) OpenCV cp <opencv-path>/release/lib/libopencv_java248.so
- * <hadoop-path>/lib/native/. otherwise will get following problem
+ * 
+ * Detect faces/eyes/.. from images encoded as Hadoop SequenceFile stored on hdfs:// or s3n://
+ *  
+ * JavaCV depedencies: 
+ * 1. Download and unzip both javacv-0.7-bin.zip and javacv-0.7-cppjars.zip
+ * 2. Specify additionalClasspathElement in pom.xml *   
+ * otherwise there will be error like: 
  * java.lang.UnsatisfiedLinkError: no opencv_java248 in java.library.path
  * 
- * JavaCV: 1. download and extract both javacv-0.7-bin.zip
- * andjavacv-0.7-cppjars.zip 2. In Eclipse: create a user library JavaCV
- * containing 1)javacv.jar 2)javacpp.jar 3) javacv-<plaform>-<os>.jar 4)
- * ffmpeg-<ver>-<platform>.jar 5) opencv-<ver>-<platform>.jar
- * 
+ * P.S. This needs to be done for each node(slave), e.g., if used in AWS EMR.
  * @author heq
  */
 // @formatter:on
 public class OpenCV {
-	private static final Logger logger = LoggerFactory.getLogger(OpenCV.class);
+	final static Logger logger = LoggerFactory.getLogger(OpenCV.class);
 	static String hostname = "localhost";
-	static {
-		try {
-			hostname = InetAddress.getLocalHost().getHostName();
-		} catch (UnknownHostException e) {
-			logger.warn("hostname is NOT resolvable by DNS. localhost is used for logging");
-		}
-	}
-	private static final Configuration conf = new Configuration();
 
-	public static void main(String[] args) throws Exception {
-		String usage = "Usage: hadoop jar ./target/*.jar com.openresearchinc.hadoop.sequencefile.OpenCV [-file <input-uri-image>|-dir <input-uri-images>] -ext <ext> ";
-		String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+	public static class Map extends MapReduceBase implements
+			Mapper<Text, BytesWritable, Text, ArrayPrimitiveWritable> {
 
-		List<String> argList = Arrays.asList(otherArgs);
-		String uri;
-		if (argList.indexOf("-file") != -1) {
-			uri = otherArgs[argList.indexOf("-file") + 1];
-			if (uri.toLowerCase().matches(".*png.*|.*jpg.*|.*gif.*")) {
-				detectFaceinPngJpgEtc(uri);
-			} else if (uri.toLowerCase().matches(".*ppm.*")) {
-				detectFaceInPPM(uri);
+		public void map(Text key, BytesWritable value,
+				OutputCollector<Text, ArrayPrimitiveWritable> output, Reporter reporter)
+				throws IOException {
+			Text outputkey = new Text();
+			ArrayPrimitiveWritable outputvalue = new ArrayPrimitiveWritable();
+			List<int[]> faces = new ArrayList<int[]>();
+			String filename = key.toString();
+			byte[] bytes = value.getBytes();
+			if (filename.toLowerCase().matches(".*png.*|.*jpg.*|.*gif.*")) {
+				BufferedImage rawimage = ImageIO.read(new ByteArrayInputStream(bytes));
+				faces = detectFace(rawimage);
+			} else if (filename.toLowerCase().matches(".*ppm.*")) {
+				ImageInputStream iis = ImageIO.createImageInputStream(new ByteArrayInputStream(
+						bytes));
+				BufferedImage rawimage = PPMImageReader.read(iis);
+				faces = detectFace(rawimage);
 			} else {
-				System.err.println("unsupported image format");
-				System.exit(2);
-			}
-		} else if (argList.indexOf("-dir") != -1) {
-			uri = otherArgs[argList.indexOf("-dir") + 1];
-			if (argList.indexOf("-ext") != -1) {
-				String ext = otherArgs[argList.indexOf("-ext") + 1];
-				detectFacesInDir(uri, ext);
-			} else {
-				System.err.println(usage);
-				System.exit(2);
-			}
-		} else {
-			System.err.println(usage);
-			System.exit(2);
-		}
-	}
-
-	public static void detectFacesInDir(String dir, String ext) throws Exception {
-		List<String> fileURIs = Util.listFiles(dir, ext);
-		for (String uri : fileURIs) {
-			if (uri.toLowerCase().matches(".*png.*|.*jpg.*|.*gif.*")) {
-				detectFaceinPngJpgEtc(uri);
-			} else if (uri.toLowerCase().matches(".*ppm.*")) {
-				detectFaceInPPM(uri);
-			} else {
-				logger.error("unsupported image format:" + ext);
+				logger.error("unsupported image formats for input: " + filename);
 				System.exit(1);
 			}
-		}
-	}
+			if (logger.isDebugEnabled())
+				logger.debug("Detect " + faces.size() + " in " + filename + " on host " + hostname);
 
-	/**
-	 * Detect face from image files in sequencefile format stored on a HDFS
-	 * directory
-	 * 
-	 * @param hdfsdir
-	 *            e.g., hdfs://master:8020/
-	 * @param ext
-	 *            e.g., seq, standard for image file in sequencefile format.
-	 * @throws IOException
-	 **/
-	public static void detectFace(String hdfsdir, String ext) throws Exception {
-		if (hdfsdir.contains("hdfs://")) {
-			conf.set("fs.defaultFS", hdfsdir);
-			FileStatus[] status = FileSystem.get(conf).listStatus(new Path(hdfsdir));
-			for (int i = 0; i < status.length; i++) {
-				Path path = status[i].getPath();
-				if (path.getName().endsWith(ext) && status[i].getLen() > 0 && !status[i].isSymlink()) {
-					logger.debug(status[i].getPath() + " " + status[i].getLen());
-					detectFaceinPngJpgEtc(path.toString());
-				}
-			}
-		} else if (hdfsdir.contains("s3://")) {
-			List<String> sequris = Util.listFiles(hdfsdir, "seq");
-			for (String uri : sequris) {
-				detectFaceinPngJpgEtc(uri);
+			for (int[] box : faces) {
+				outputkey.set(filename);
+				outputvalue.set(box);
+				output.collect(outputkey, outputvalue);
 			}
 		}
 	}
 
 	/**
-	 * Detect face from image in PNG, JPG, BMP etc. where supported by Jave
-	 * ImageIO out-of-box
+	 * A place holder reducer for further processing of detected face.
 	 * 
-	 * @param uri
-	 * @return
-	 * @throws IOException
-	 * @throws Exception
+	 * 
 	 */
-	public static int detectFaceinPngJpgEtc(String uri) {
-		Map<Text, byte[]> imagesequnce = null;
-		if (uri.startsWith("s3n://"))
-			try {
-				imagesequnce = Util.readSequenceFileFromS3(uri);
-			} catch (IOException e) {
-				logger.warn("Cannot read SequnceFile from S3: " + uri);
-				return -1;
+	public static class Reduce extends MapReduceBase implements
+			Reducer<Text, ArrayPrimitiveWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterator<ArrayPrimitiveWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+			int sum = 0;
+			while (values.hasNext()) {
+				sum++;
 			}
-		else if (uri.startsWith("hdfs://"))
-			try {
-				imagesequnce = Util.readSequenceFileFromHDFS(uri);
-			} catch (IOException e) {
-				logger.warn("Cannot read SequnceFile from HDFS:" + uri);
-				return -1;
-			}
-		else {
-			logger.error("only hdfs:// and s3n;// sequncefile accesses are implemented");
-			System.exit(1);
+			output.collect(key, new IntWritable(sum));
 		}
-
-		for (Map.Entry<Text, byte[]> entry : imagesequnce.entrySet()) {
-			String filename = entry.getKey().toString();
-
-			try {
-				BufferedImage rawimage = ImageIO.read(new ByteArrayInputStream(entry.getValue()));
-				int faces = detectFace(rawimage);
-				logger.info("hostname=" + hostname + " filename=" + filename + " faces=" + faces);
-				return faces; // 0: no face; 1: one face, ...
-			} catch (IOException e) {
-				logger.warn("Cannot read bytes into image");
-				return -1;
-			}
-		}
-		return -1; // indicate error in image sequencefile
 	}
 
-	/**
-	 * Detect face from image in PPM format
-	 * 
-	 * @param uri
-	 * @return
-	 */
-	public static int detectFaceInPPM(String uri) {
-		Map<Text, byte[]> imagesequnce = null;
-		if (uri.startsWith("s3n://"))
-			try {
-				imagesequnce = Util.readSequenceFileFromS3(uri);
-			} catch (IOException e) {
-				logger.warn("Cannot read SequnceFile from S3: " + uri);
-				return -1;
-			}
+	public static void main(String[] args) throws Exception {
+		String usage = "Usage: hadoop jar ./target/bin2seq*.jar com.openresearchinc.hadoop.sequencefile.OpenCV  <input-uri-images-on-hdfs-s3> <output-uri-of-detected-boxes-faces-in-each-image>";
+		hostname = InetAddress.getLocalHost().getHostName();
 
-		else if (uri.startsWith("hdfs://"))
-			try {
-				imagesequnce = Util.readSequenceFileFromHDFS(uri);
-			} catch (IOException e) {
-				logger.warn("Cannot read SequnceFile from HDFS:" + uri);
-				return -1;
-			}
-		else {
-			logger.error("only hdfs:// and s3n;// sequncefile accesses are implemented");
-			System.exit(1);
-		}
-		for (Map.Entry<Text, byte[]> entry : imagesequnce.entrySet()) {
-			String filename = entry.getKey().toString();
-			try {
-				ImageInputStream iis = ImageIO.createImageInputStream(new ByteArrayInputStream(entry.getValue()));
-				BufferedImage rawimage = PPMImageReader.read(iis);
-				int faces = detectFace(rawimage);
-				logger.info("hostname= " + hostname + " filename= " + filename + " faces= " + faces);
-				return faces;
-			} catch (IOException e) {
-				logger.warn("Cannot read bytes into image");
-				return -1;
-			}
-		}
-		return -1; // indicate error in image sequencefile
+		JobConf job = new JobConf(OpenCV.class);
+
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
+
+		job.setInputFormat(TextInputFormat.class);
+		job.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+		JobClient.runJob(job);
 	}
 
-	public static int detectFace(BufferedImage rawimage) {
-		String faceClassifierPath = new File(OpenCV.class.getResource("/haarcascade_frontalface_alt.xml").getFile())
-				.getAbsolutePath();
-		int faces = detect(rawimage, faceClassifierPath);
-		logger.debug("faces=" + faces);
-		return faces;
+	public static List<int[]> detectFace(BufferedImage rawimage) {
+		String faceClassifierPath = new File(OpenCV.class.getResource(
+				"/haarcascade_frontalface_alt.xml").getFile()).getAbsolutePath();
+		return detect(rawimage, faceClassifierPath);
 	}
 
-	public static int detectEye(BufferedImage rawimage) {
-		String eyeClassifierPath = new File(OpenCV.class.getResource("/haarcascade_eye.xml").getFile())
-				.getAbsolutePath();
-		int eyes = detect(rawimage, eyeClassifierPath);
-		logger.debug("eyes=" + eyes);
-		return eyes;
+	public static List<int[]> detectEye(BufferedImage rawimage) {
+		String eyeClassifierPath = new File(OpenCV.class.getResource("/haarcascade_eye.xml")
+				.getFile()).getAbsolutePath();
+		return detect(rawimage, eyeClassifierPath);
 	}
 
-	private static int detect(BufferedImage rawimage, String classifierPath) {
+	static List<int[]> detect(BufferedImage rawimage, String classifierPath) {
+		// store x-y of the box of detected face
+		List<int[]> facebox = new ArrayList<int[]>();
+
 		CvHaarClassifierCascade classifier = new CvHaarClassifierCascade(cvLoad(classifierPath));
 		IplImage origImg = IplImage.createFrom(rawimage);
 		CvMemStorage storage = CvMemStorage.create();
-		CvSeq target = cvHaarDetectObjects(origImg, classifier, storage, 1.1, 3, CV_HAAR_DO_CANNY_PRUNING);
+		CvSeq target = cvHaarDetectObjects(origImg, classifier, storage, 1.1, 3,
+				CV_HAAR_DO_CANNY_PRUNING);
 		cvClearMemStorage(storage);
 
 		for (int i = 0; i < target.total(); i++) {
 			CvRect r = new CvRect(cvGetSeqElem(target, i));
+			facebox.add(new int[] { r.x(), r.y(), r.x() + r.width(), r.y() + r.height() });
 			if (logger.isDebugEnabled())
-				logger.debug("x=" + r.x() + " y=" + r.y() + " x+w=" + r.x() + r.width() + " y+h=" + r.y() + r.height());
+				logger.debug("x=" + r.x() + " y=" + r.y() + " x+w=" + r.x() + r.width() + " y+h="
+						+ r.y() + r.height());
 		}
 		if (logger.isDebugEnabled())
-			logger.debug("image width=" + rawimage.getWidth() + " height=" + rawimage.getHeight()
-					+ " num of identified object=" + target.total());
-		return target.total();
+			logger.debug("Num of detected objects=" + target.total());
+		return facebox;
 	}
 
 }
