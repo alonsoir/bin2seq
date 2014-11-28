073feb50e94921bda02f65fa98a3ad4d5d767cd7
Qiming He <Qiming.He@openresearchinc.com>
2014-11-27 20:34:35 -0800

    update to use new python aws api tools

--- scripts/emr-launch.sh
+++ scripts/emr-launch.sh
@@ -1,16 +1,18 @@
 
 export bootstrap="s3://ori-emrscript/bin2seq-emr-bootstrap.sh"
 #export args="-m,mapred.child.java.opts=-Xmx4096m"
-export args="-namenode-heap-size=6144,--jobtracker-heap-size=6144,--tasktracker-heap-size=6144,--namenode-opts=-XX:+UseCompressedOops,--jobtracker-opts=-XX:+UseCompressedOops,--tasktracker-opts=-XX:+UseCompressedOops"
+export args="--namenode-heap-size=6144,--jobtracker-heap-size=6144,--tasktracker-heap-size=6144,--namenode-opts=-XX:+UseCompressedOops,--jobtracker-opts=-XX:+UseCompressedOops,--tasktracker-opts=-XX:+UseCompressedOops"
 export clustername="test"
+export amiversion="3.3.1"
 export masterinstance="m3.xlarge"
 export slaveinstance="m3.xlarge"
 export slaves="1"
-export bidprice="0.04"
+export bidprice=`aws ec2 describe-spot-price-history --instance-types m3.xlarge --product-descriptions Linux/UNIX |grep SpotPrice |sort |head -n 1 |cut -d':' -f2 | sed -e 's/"//g' -e 's/,//g' |awk '{print $1 + 0.01}'  |cut -c1-4` #lowest bid + $0.01
+echo "biding price="$bidprice
 
-elastic-mapreduce --create --bootstrap-action $bootstrap --args $args --name $clustername --alive --hadoop-version 2.2.0 --ami-version latest --instance-group master --instance-type $masterinstance --instance-count 1 --bid-price $bidprice --instance-group core --instance-type $slaveinstance --instance-count $slaves  --bid-price $bidprice
+aws emr create-cluster --enable-debugging --log-uri s3://ori-tmp/emr-log  --ec2-attributes KeyName=gsg-keypair --ami-version $amiversion --bootstrap-actions Path=$bootstrap,Name="Set heap size",Args=[$args] --instance-groups InstanceGroupType=MASTER,InstanceCount=1,BidPrice=$bidprice,Name=Master,InstanceType=$masterinstance InstanceGroupType=CORE,BidPrice=$bidprice,Name=Slave,InstanceCount=$slaves,InstanceType=$slaveinstance 
 
-export jobid=`elastic-mapreduce --list --active |awk '{print $1}'`
-elastic-mapreduce --jobflow $jobid --jar /home/hadoop/lib/emr-s3distcp-1.0.jar --args '--src,s3://ori-msd10k-seq/,--dest,hdfs:///msd10k'
-elastic-mapreduce --jobflow $jobid --jar /home/hadoop/lib/emr-s3distcp-1.0.jar --args '--src,s3://ori-colorferet-seq/,--dest,hdfs:///colorferet'
+#export jobid=`elastic-mapreduce --list --active |awk '{print $1}'`
+#elastic-mapreduce --jobflow $jobid --jar /home/hadoop/lib/emr-s3distcp-1.0.jar --args '--src,s3://ori-msd10k-seq/,--dest,hdfs:///msd10k'
+#elastic-mapreduce --jobflow $jobid --jar /home/hadoop/lib/emr-s3distcp-1.0.jar --args '--src,s3://ori-colorferet-seq/,--dest,hdfs:///colorferet'
 
