9f38830cb76ffd7b4a89198de1b2c2c17e1a1f14
Qiming He <Qiming.He@openresearchinc.com>
2014-11-27 20:32:42 -0800

    pass 3 test cases for different data types

--- run.sh
+++ run.sh
@@ -1,10 +1,45 @@
 mvn clean compile package -DskipTests=true
-hadoop fs -rmr /output
-hadoop fs -rmr /colorferetsub
+export HDFSOUT="/output"
 export MVNJARS=`find $HOME/.m2/repository/ -name "*.jar" |while read jar; do echo $jar; done |tr '\n' ','`
-export LIBJARS="$MVNJARS,$JAVACV_BIN_HOME/javacv.jar,$JAVACV_BIN_HOME/javacpp.jar,$JAVACV_BIN_HOME/javacv-linux-x86_64.jar,$JAVACV_CPP_HOME/opencv-2.4.8-linux-x86_64.jar,$JAVACV_CPP_HOME/ffmpeg-2.1.1-linux-x86_64.jar"
+export LIBJARS="$MVNJARS,$HADOOP_HOME/lib/native"
 export HADOOP_CLASSPATH=`echo ${LIBJARS} | sed s/,/:/g`
 export CLASSPATH=`echo ${LIBJARS} | sed s/,/:/g`
+
+#Processing the file compute pr mean/min/max
+hadoop fs -rm -f -r /output
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.NetCDF -libjars ${LIBJARS} hdfs:///nc-seq hdfs:///output
+exit
+
+#Passed: copy a nasanex netcdf file to HDFS
+hadoop fs -rm -f -r /nc-seq
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://nasanex/NEX-DCP30/BCSD/rcp26/mon/atmos/pr/r1i1p1/v1.0/CONUS/pr_amon_BCSD_rcp26_r1i1p1_CONUS_HadGEM2-ES_200512-200512.nc -ext nc -out hdfs:///nc-seq -codec snappy
+exit
+
+#Passed: Identify tempo from *.h5.seq
+hadoop fs -rm -f -r /output
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.HDF5 -libjars ${LIBJARS} hdfs:///msd10k-seq/  /output -attr tempo
+exit
+
+#Passed: Copy small set of MSD *.h5 from S3 to HDFS
+hadoop fs -rm -f -r /msd10k-seq
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3:///ori-msd10k-h5/data/A/A/A/ -ext h5 -out hdfs:///msd10k-seq -codec snappy
+exit 
+
+#Passed: Copy small set of ppm.bz from S3 to HDFS 
+hadoop fs -rm -f -r /colorferet-seq
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://ori-colorferetsubset/00001/ -ext ppm.bz2 -out hdfs:///colorferet-seq -codec snappy 
+exit
+
+#Passed: Open CV Face Detection 
+hadoop fs -rm -f -r /output
+hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.OpenCV -libjars $LIBJARS hdfs:///colorferet-seq/ hdfs:///output  
+exit
+
+
+
+
+
+#passed 
 
 #passed test cases
 #hadoop jar ./target/bin2seq-0.1.jar com.openresearchinc.hadoop.sequencefile.Util -pack -in s3://ori-msd10k-h5/data/A/A/ -ext h5 -out /msd10k-seq -codec snappy
