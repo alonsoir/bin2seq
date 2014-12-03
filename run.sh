7f91c593591493e17f31fab7df3e215816053c3a
Qiming He <Qiming.He@openresearchinc.com>
2014-12-02 20:02:12 -0800

    output seq to s3 and use switch for test cases

--- run.sh
+++ run.sh
@@ -7,42 +7,37 @@ export CLASSPATH=`echo ${LIBJARS} | sed s/,/:/g`
 export HEAPSIZE="10240"
 export CMD="hadoop jar ./target/bin2seq-${Ver}.jar" 
 
-#Passed: Simple test case to retrieve geotiff coordinate corners  
+case "$1" in
+        ppm)
+aws s3 rm --recursive s3://ori-bin2seq/colorferet-seq
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://ori-colorferetsubset/00001/ -ext ppm.bz2 -out s3://ori-bin2seq/colorferet-seq -codec snappy 
 hadoop fs -rm -f -r /output
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.GeoTiff -libjars ${LIBJARS} hdfs:///landsat-seq hdfs:///output
-exit
-
-#Passed: copy nasanex Landsat geotif to HDFS
-hadoop fs -rm -f -r /landsat-seq
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://nasanex/Landsat/gls/1975/001/026/ -ext tif -out hdfs:///landsat-seq -codec snappy
-exit
-
-#Passed: Processing the file compute pr mean/min/max
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.OpenCV -libjars $LIBJARS s3://ori-bin2seq/colorferet-seq hdfs:///output  
+            ;;
+         
+        tif)
+aws s3 rm --recursive s3://ori-bin2seq/landsat-seq
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://nasanex/Landsat/gls/1975/001/026/ -ext tif -out s3://ori-bin2seq/landsat-seq -codec snappy
 hadoop fs -rm -f -r /output
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.NetCDF -libjars ${LIBJARS} hdfs:///nc-seq hdfs:///output
-exit
-
-#Passed: copy a nasanex netcdf file to HDFS
-hadoop fs -rm -f -r /nc-seq
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://nasanex/NEX-DCP30/BCSD/rcp26/mon/atmos/pr/r1i1p1/v1.0/CONUS/pr_amon_BCSD_rcp26_r1i1p1_CONUS_HadGEM2-ES_200512-200512.nc -ext nc -out hdfs:///nc-seq -codec snappy
-exit
-
-#Passed: Identify tempo from *.h5.seq
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.GeoTiff -libjars ${LIBJARS} s3://ori-bin2seq/landsat-seq hdfs:///output
+            ;;
+         
+        netcdf)
+aws s3 rm --recursive s3://ori-bin2seq/netcdf-seq
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://nasanex/NEX-DCP30/BCSD/rcp26/mon/atmos/pr/r1i1p1/v1.0/CONUS/pr_amon_BCSD_rcp26_r1i1p1_CONUS_HadGEM2-ES_200512-200512.nc -ext nc -out s3://ori-bin2seq/netcdf-seq -codec snappy #large size problem??
 hadoop fs -rm -f -r /output
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.HDF5 -libjars ${LIBJARS} hdfs:///msd10k-seq/  /output -attr tempo
-exit
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.NetCDF -libjars ${LIBJARS} s3://ori-bin2seq/netcdf-seq hdfs:///output
+            ;;
 
-#Passed: Copy small set of MSD *.h5 from S3 to HDFS
-hadoop fs -rm -f -r /msd10k-seq
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3:///ori-msd10k-h5/data/A/A/A/ -ext h5 -out hdfs:///msd10k-seq -codec snappy
-exit 
-
-#Passed: Copy small set of ppm.bz from S3 to HDFS 
-hadoop fs -rm -f -r /colorferet-seq
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3://ori-colorferetsubset/00001/ -ext ppm.bz2 -out hdfs:///colorferet-seq -codec snappy 
-exit
-
-#Passed: Open CV Face Detection 
+        hdf5)
+aws s3 rm --recursive s3://ori-bin2seq/hdf5-seq
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.Util -libjars $LIBJARS -in s3:///ori-msd10k-h5/data/A/A/A/ -ext h5 -out s3://ori-bin2seq/hdf5-seq -codec snappy
 hadoop fs -rm -f -r /output
-HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.OpenCV -libjars $LIBJARS hdfs:///colorferet-seq/ hdfs:///output  
-exit
+HADOOP_HEAPSIZE=$HEAPSIZE $CMD com.openresearchinc.hadoop.sequencefile.HDF5 -libjars ${LIBJARS} s3://ori-bin2seq/hdf5-seq  /output -attr tempo
+            ;;
+         
+        *)
+            echo $"Usage: $0 {ppm|tif|netcdf|hdf5}"
+            exit 1
+esac
+
